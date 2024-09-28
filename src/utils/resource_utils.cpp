6fd14a6ac26434bf1edf42c72e3a29d99acb6bfb
Egor Meganov <e.meganov@innopolis.university>
2024-09-28 22:14:25 +0300

    Week 1
    Signed-off-by: Egor Meganov <e.meganov@innopolis.university>

--- src/utils/resource_utils.cpp
+++ src/utils/resource_utils.cpp
@@ -13,28 +13,27 @@ using namespace cg::utils;
 std::string view_command(const std::filesystem::path& path)
 {
 #ifdef __APPLE__
-	return std::string("open ").append(path.string());
+  return std::string("open ").append(path.string());
 #endif
 #ifdef _WIN32
-	return std::string("start ").append(path.string());
+  return std::string("start ").append(path.string());
 #endif
-	return "";
+  return "";
 }
 
-void save_resource(cg::resource<cg::unsigned_color>& render_target, const std::filesystem::path& filepath)
+void cg::utils::save_resource(cg::resource<cg::unsigned_color>& render_target, std::filesystem::path filepath)
 {
-	int width = static_cast<int>(render_target.get_stride());
-	int height = static_cast<int>(render_target.count()) / width;
+  int width = static_cast<int>(render_target.get_stride());
+  int height = static_cast<int>(render_target.count()) / width;
 
-	int result = stbi_write_png(
-			filepath.string().c_str(), width, height, 3, render_target.get_data(),
-			width * sizeof(cg::unsigned_color));
+  int result = stbi_write_png(
+      filepath.string().c_str(), width, height, 3, render_target.get_data(),
+      width * sizeof(cg::unsigned_color));
 
-	if (result != 1)
-		THROW_ERROR("Can't save the resource");
+  if (result != 1)
+    THROW_ERROR("Can't save the resource");
 
-	auto command = view_command(filepath);
-	if (!command.empty())
-		std::system(command.c_str());
+  auto command = view_command(filepath);
+  if (!command.empty())
+    std::system(command.c_str());
 }
-
